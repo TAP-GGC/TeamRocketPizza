1f149303d55c4aa753329a489faf3082d58e8bdc
danielto2910 <dto1@ggc.edu>
2024-09-14 17:33:45 -0400

    updateTowerSprite/Prefab

--- code/Pizza Heist Game/Assets/Script/ProjectileController.cs
+++ code/Pizza Heist Game/Assets/Script/ProjectileController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
@@ -8,6 +9,7 @@ public class ProjectileController : MonoBehaviour
     
     [Header("References")]
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform rotPoint;
 
     [Header("Attribute")]
     [SerializeField] private float projSpeed = 1f;
@@ -24,8 +26,22 @@ public class ProjectileController : MonoBehaviour
     void FixedUpdate(){
         if(!target) return;
         Vector2 direction = (target.position - transform.position).normalized;
-
+        RotateTowardsTarget();
+        
+        
         rb.velocity = direction * projSpeed;
+    }
+
+    private void RotateTowardsTarget(){ 
+        float angle = Mathf.Atan2( // Algorithm to track angle rotation towards target
+        target.position.y - transform.position.y,
+        target.position.x - transform.position.x) 
+        * Mathf.Rad2Deg - 90f;
+
+        // Convert to Quaternion
+        // Only make the rotation happen on the z-axis
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f,0f,angle)); 
+        rotPoint.rotation = Quaternion.RotateTowards(rotPoint.rotation,targetRotation,235f * Time.deltaTime);
     }
 
     public void SetTarget(Transform _target){
