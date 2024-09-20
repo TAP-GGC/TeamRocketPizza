651de9c847e05d9b1c72487976bd671e575f4a89
Nikki <nikkithao127@gmail.com>
2024-09-20 13:06:01 -0400

    TextWriter updated to support multiple messages

--- code/Pizza Heist Game/Assets/Script/TextWriter.cs
+++ code/Pizza Heist Game/Assets/Script/TextWriter.cs
@@ -6,38 +6,60 @@ using UnityEngine.UI;
 public class TextWriter : MonoBehaviour
 {
 
-    private Text uiText;
-    private string textToWrite;
-    private int characterIndex;
-    private float timePerCharacter;
-    private float timer;
-    private bool invisibleCharacters;
+    private List<TextWriterSingle> textWriterSingleList;
+
+    private void Awake() {
+        textWriterSingleList = new List<TextWriterSingle>();
+    }
 
     public void addWriter(Text uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters) {
-        this.uiText = uiText;
-        this.textToWrite = textToWrite;
-        this.timePerCharacter = timePerCharacter;
-        this.invisibleCharacters = invisibleCharacters;
-        characterIndex = 0;
+        textWriterSingleList.Add(new TextWriterSingle(uiText, textToWrite, timePerCharacter, invisibleCharacters));
     }
 
     private void Update() {
-        if(uiText != null) {
-            timer -= Time.deltaTime;
-            while (timer <= 0f) {
-                //Display next character
-                timer += timePerCharacter;
-                characterIndex++;
-                string text = textToWrite.Substring(0, characterIndex);
-                if (invisibleCharacters) {
-                    text += "<color=#00000000>" + textToWrite.Substring(0, characterIndex) + "</color>";
-                }
-                uiText.text = text;
+        for (int i = 0; i <textWriterSingleList.Count; i++) {
+            textWriterSingleList[i].Update();
+        }
+    }
 
-                if (characterIndex >= textToWrite.Length) {
-                    //Entire string displayed
-                    uiText = null;
-                    return;
+    
+    //Represents a single TextWriter instance
+
+    public class TextWriterSingle {
+
+            private Text uiText;
+        private string textToWrite;
+        private int characterIndex;
+        private float timePerCharacter;
+        private float timer;
+        private bool invisibleCharacters;
+
+        public TextWriterSingle(Text uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters) {
+            this.uiText = uiText;
+            this.textToWrite = textToWrite;
+            this.timePerCharacter = timePerCharacter;
+            this.invisibleCharacters = invisibleCharacters;
+            characterIndex = 0;
+        }
+
+        public void Update() {
+            if(uiText != null) {
+                timer -= Time.deltaTime;
+                while (timer <= 0f) {
+                    //Display next character
+                    timer += timePerCharacter;
+                    characterIndex++;
+                    string text = textToWrite.Substring(0, characterIndex);
+                    if (invisibleCharacters) {
+                        text += "<color=#00000000>" + textToWrite.Substring(0, characterIndex) + "</color>";
+                    }
+                    uiText.text = text;
+
+                    if (characterIndex >= textToWrite.Length) {
+                        //Entire string displayed
+                        uiText = null;
+                        return;
+                    }
                 }
             }
         }
