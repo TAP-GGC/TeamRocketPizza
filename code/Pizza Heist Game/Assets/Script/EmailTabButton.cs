be9f5f698e802ee37061c31f4ac5fceb20ad79be
bramoscazares <bramoscazares@gmail.com>
2024-09-26 13:19:17 -0400

    Email Tab Scripts

--- code/Pizza Heist Game/Assets/Script/EmailTabButton.cs
+++ code/Pizza Heist Game/Assets/Script/EmailTabButton.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class EmailTabButton : MonoBehaviour
+[RequireComponent(typeof(Image))]
+public class EmailTabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
 {
+    public EmailTabGroup tabGroup;
+    public Image background;
+
+    public Color activeColor;
+    public Color hoverColor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        background = GetComponent<Image>();
+        tabGroup.Subscribe(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
     }
 }
--- code/Pizza Heist Game/Assets/Script/EmailTabGroup.cs
+++ code/Pizza Heist Game/Assets/Script/EmailTabGroup.cs
@@ -1,13 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EmailTabGroup : MonoBehaviour
 {
+
+    public List<EmailTabButton> tabButtons;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void Subscribe(EmailTabButton button)
+    {
+        if (tabButtons == null)
+        {
+            tabButtons = new List<EmailTabButton>();
+        }
+
+        tabButtons.Add(button);
+    }
+
+
+    public void OnTabEnter(EmailTabButton button)
+    {
+        ResetTabs();
+        if (button.background != null)
+        {
+            button.background.color = button.hoverColor;
+        }
+    }
+
+    public void OnTabExit(EmailTabButton button)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(EmailTabButton button)
+    {
+        if (button.background != null)
+        {
+            button.background.color = button.activeColor;
+        }
+        ResetTabs();
+        button.background.color = button.activeColor;
+    }
+
+    public void ResetTabs()
+    {
+        foreach (EmailTabButton button in tabButtons)
+        {
+            if (button.background != null)
+            {
+                button.background.color = button.inactiveColor;
+            }
+        }
     }
 
     // Update is called once per frame
