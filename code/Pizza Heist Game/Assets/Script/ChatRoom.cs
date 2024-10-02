84f36e9e629c60e885774e45e9c2da1433778cfd
Nikki <nikkithao127@gmail.com>
2024-10-02 15:16:36 -0400

    Added fading text animations to desktop

--- code/Pizza Heist Game/Assets/Script/ChatRoom.cs
+++ code/Pizza Heist Game/Assets/Script/ChatRoom.cs
@@ -8,17 +8,24 @@ public class ChatRoom : MonoBehaviour
 {
     public Button startChatButton;
     public Image chatImage;
+    public Image bossIcon;
     private Text messageText;
+    private Text clickToContinue;
     private TextWriter.TextWriterSingle textWriterSingle;
     private int currentMessageIndex = 0;
+    public float delay = 45f;
     private string[] messageArray;
 
     private void Awake() {
     messageText = transform.Find("message").Find("bossMessage").GetComponent<Text>();
+    clickToContinue = transform.Find("message2").Find("clickToContinue").GetComponent<Text>();
 
         messageArray = new string[] {
             "Welcome new recruit.",
-            "I am your boss and I will be informing you of your tasks.",
+            "Congratulations on logging onto your desktop for the first time.",
+            "Hopefully you made a secure password.",
+            "I will only communicate with you through chat messages.",
+            "Do not expect any emails from me.",
             "Our pizza shop is in danger of being hacked.",
             "You will ensure the safety of our network starting with setting up the anti-virus.",
             "Click the anti-virus folder to begin your first task."
@@ -27,19 +34,25 @@ public class ChatRoom : MonoBehaviour
         startChatButton.onClick.AddListener(OnStartChatClicked);
         messageText.gameObject.SetActive(false);
         chatImage.gameObject.SetActive(false);
+        bossIcon.gameObject.SetActive(false);
+        clickToContinue.gameObject.SetActive(false);
     }
 
-        private void OnStartChatClicked() {
+    private void OnStartChatClicked() {
 
         startChatButton.gameObject.SetActive(false);
 
         messageText.gameObject.SetActive(true);
         chatImage.gameObject.SetActive(true);
+        bossIcon.gameObject.SetActive(true);
+
+        ShowNextMessage();
 
         if (currentMessageIndex < messageArray.Length) {
             string initialMessage = messageArray[currentMessageIndex];
             currentMessageIndex++;
             textWriterSingle = TextWriter.AddWriter_Static(messageText, initialMessage, .05f, true, true);
+            StartCoroutine(ShowClickToContinueAfterTextIsFinished());
         }
 
         transform.Find("message").GetComponent<Button_UI>().ClickFunc = () => {
@@ -47,11 +60,39 @@ public class ChatRoom : MonoBehaviour
                 textWriterSingle.WriteAllAndDestroy();
             } else {
                 if (currentMessageIndex < messageArray.Length) {
+                    HideClickToContinue();
                     string message = messageArray[currentMessageIndex];
                     currentMessageIndex++;
                     textWriterSingle = TextWriter.AddWriter_Static(messageText, message, .05f, true, true);
+                    StartCoroutine(ShowClickToContinueAfterTextIsFinished());
                 }
             }
         };
+    }
+
+    private void ShowNextMessage() {
+    if (currentMessageIndex < messageArray.Length) {
+        string message = messageArray[currentMessageIndex];
+        currentMessageIndex++;
+        textWriterSingle = TextWriter.AddWriter_Static(messageText, message, .05f, true, true);
+        StartCoroutine(ShowClickToContinueAfterTextIsFinished());
+    }
 }
+
+    private IEnumerator ShowClickToContinueAfterTextIsFinished() {
+        // Wait until the text finishes writing
+        while (textWriterSingle != null && textWriterSingle.IsActive()) {
+            yield return null;
+        }
+
+        // Now wait for the specified delay
+        yield return new WaitForSeconds(delay);
+
+        Debug.Log("Showing 'click to continue' text.");
+        clickToContinue.gameObject.SetActive(true); // Show the "click to continue" text
+    }
+
+    private void HideClickToContinue() {
+        clickToContinue.gameObject.SetActive(false); // Hide the "click to continue" text when clicked
+    }
 }
